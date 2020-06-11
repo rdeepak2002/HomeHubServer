3a75b4ee1e4fc3ca5640fd64fe2e1612db12ee27
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-11 14:18:05 -0700

    reboots on update

--- index.js
+++ index.js
@@ -33,6 +33,22 @@ app.post('/command', (req, res) => {
       res.send({ message: stderr })
       return
     }
+
+    // reboot if system is not up to date
+    if(stdout.trim() != 'Already up to date.') {
+      exec('sudo reboot', (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`)
+          return
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`)
+          return
+        }
+        console.log(`stdout: ${stdout}`)
+      })
+    }
+
     res.send({ message: stdout })
   })
 })
